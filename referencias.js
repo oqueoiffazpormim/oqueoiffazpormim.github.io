f67ea5eef71fcc1093979ea575598e4f4ff0fa3f
João Vitor <34198932+jvitorsl@users.noreply.github.com>
2019-05-07 21:29:16 -0300

    Grande atualização 1 - Junção das partes (Laura)

---
+++ referencias.js
@@ -0,0 +1,22 @@
+function biologia(){
+    window.open('https://www.ifpb.edu.br/itaporanga/noticias/2018/09/robotica-do-campus-se-classifica-para-etapa-estadual-da-olimpiada-brasileira-de-robotica')
+}
+function informatica(){
+    window.open('http://www.ifpb.edu.br/campinagrande/noticias/2018/11/estudantes-do-campus-campina-ganham-ouro-prata-e-bronze-em-olimpiada')
+}
+function robotica(){
+    window.open('https://www.ifpb.edu.br/itaporanga/noticias/2018/09/robotica-do-campus-se-classifica-para-etapa-estadual-da-olimpiada-brasileira-de-robotica')
+}
+function fisica(){
+    window.open('https://estudante.ifpb.edu.br/noticias/campus-campina-premia-vencedores-da-olimpiada-campinense-de-fisica')
+}
+function historia(){
+    window.open('https://www.ifpb.edu.br/campinagrande/noticias/2018/08/ifpb-lidera-numero-de-finalistas-em-olimpiada-nacional-de-historia')
+}
+function quimica(){
+    window.open('https://www.ifpb.edu.br/joaopessoa/noticias/2017/11/estudantes-do-campus-joao-pessoa-conquistam-medalhas-na-olimpiada-paraibana-de-quimica')
+}
+function matematica(){
+    window.open('http://www.ifpb.edu.br/campinagrande/noticias/2016/12/alunos-do-campus-campina-grande-sao-premiados-na-olimpiada-pessoense-de-matematica')
+}
+
