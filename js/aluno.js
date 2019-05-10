41ef81f90946cb57c215a45bfc77f47c146d68bf
João Vitor <34198932+jvitorsl@users.noreply.github.com>
2019-05-10 02:17:40 -0300

    .js de aluno

---
+++ js/aluno.js
@@ -0,0 +1,7 @@
+function google(){
+    window.open('http://editor.ifpb.edu.br/campi/campina-grande/noticias/aplicativo-criado-por-estudantes-de-campina-e-selecionado-em-concurso-nacional-1');
+}
+
+function polonia(){
+    window.open('https://g1.globo.com/pb/paraiba/noticia/paraibano-faz-vaquinha-para-estudar-na-polonia-e-ser-primeiro-universitario-da-familia.ghtml');
+}
