8baace3bf9ff818518cb05818f0a47545c1f1db7
oqueoiffazpormim <50335415+oqueoiffazpormim@users.noreply.github.com>
2019-05-13 15:38:07 -0300

    menu e rodapé

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+$(function () {
+    var includes = $('[data-include]');
+    jQuery.each(includes, function () {
+        var file = 'views/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
